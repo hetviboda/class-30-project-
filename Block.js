7c275c019b29e988051aacd1dc3bf729c3137294
hetviboda <85609337+hetviboda@users.noreply.github.com>
2021-06-21 19:50:08 +0530

    Update Block.js

--- Block.js
+++ Block.js
@@ -16,12 +16,12 @@ class Block{
       if(this.body.speed <3){
       var angle = this.body.angle;
       var pos= this.body.position;
-      push();
+      
       translate(pos.x, pos.y);
       rotate(angle);
       rectMode(CENTER);
       rect(0,0,this.width, this.height);
-      pop();
+      
     }else{
       World.remove(world, this.body);
       push();
